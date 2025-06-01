a589e6807195b2035e5a71aa1d3412c8635b2c20
Rakesh Jangid <rakesh@jangid.co.in>
2025-06-01 17:41:57 +0530

    update first

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
 <footer class="fixed w-full bottom-0 bg-zinc-50 mt-8 text-center dark:bg-neutral-700 lg:text-left">
 	<div class="bg-black/5 p-4 text-center text-surface dark:text-white w-full bottom-0">
 	  © 2025:
-	  <a href="https://www.kotharicollege.org/">Kothari Mahila Mahavidyalaya, Sankhantal.</a>
+	  <a href="https://www.kotharicollege.org/">Kothari Mahila Mahavidyalaya, Rajgarh.</a>
 	</div>
   </footer>
 </template>
